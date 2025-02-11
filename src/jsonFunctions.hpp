68013c4de81c77bcbcc4c5f6cc43eabb42cae9ae
Reuben Rissler <benreu.rissler@gmail.com>
2025-02-11 10:48:14 -0500

    add nlohmann-json library requirement

--- src/jsonFunctions.hpp
+++ src/jsonFunctions.hpp
@@ -25,7 +25,7 @@
 // ESP32 pollutes the env with macros for all possible binary values starting with "B" (pe "B1 = 1", "B1111=7"...)
 // undef them, as json.hpp defines a template argument with B1
 #undef B1
-#include <../lib/json/json.hpp>
+#include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
 #pragma once
