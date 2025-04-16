8b886843e398c3e1ec5a03cc4b3da88d6ac0a610
Reuben Rissler <benreu.rissler@gmail.com>
2025-04-15 20:45:42 -0400

    refactor network.cpp
    correct indentation to single tab per block

--- src/network.cpp
+++ src/network.cpp
@@ -9,35 +9,35 @@ String apName;
 bool WiFiWasConnected = false;
 
 void WiFiStationGotIP( WiFiEvent_t event, WiFiEventInfo_t info ){
-    IPAddress myIP = WiFi.localIP();
-    if( myIP[3] != 83 ){
-        IPAddress gwIP = WiFi.gatewayIP();
-        delay( 10 );
-        myIP[3] = 83;
-        if( !WiFi.config( myIP, gwIP, IPAddress( 255, 255, 255, 0 ), gwIP )){
-          Serial.println( "STA Failed to configure" );
-        }
-        Serial.println( "Switching off AP, station only" );
-        WiFi.softAPdisconnect( true );
-        WiFi.mode( WIFI_MODE_STA );
+  IPAddress myIP = WiFi.localIP();
+  if( myIP[3] != 83 ){
+    IPAddress gwIP = WiFi.gatewayIP();
+    delay( 10 );
+    myIP[3] = 83;
+    if( !WiFi.config( myIP, gwIP, IPAddress( 255, 255, 255, 0 ), gwIP )){
+      Serial.println( "STA Failed to configure" );
     }
-    digitalWrite( lightbarConfig.gpioApMode, HIGH );
-    WiFiWasConnected = true;
+    Serial.println( "Switching off AP, station only" );
+    WiFi.softAPdisconnect( true );
+    WiFi.mode( WIFI_MODE_STA );
+  }
+  digitalWrite( lightbarConfig.gpioApMode, HIGH );
+  WiFiWasConnected = true;
 }
 
 void WiFiStationDisconnected( WiFiEvent_t event, WiFiEventInfo_t info ){
-    digitalWrite( lightbarConfig.gpioApMode, LOW );
-    if( WiFiWasConnected == true ){
-      WiFi.disconnect( true );
-      if( !WiFi.config( INADDR_NONE, INADDR_NONE, INADDR_NONE )){
-        Serial.println( "STA Failed to unset configuration" );
-      }
-      delay( 25 );
-      WiFi.begin( lightbarConfig.ssid, lightbarConfig.password );
-      Serial.println( "reconnecting" );
-    } else {
-      WiFi.reconnect();
+  digitalWrite( lightbarConfig.gpioApMode, LOW );
+  if( WiFiWasConnected == true ){
+    WiFi.disconnect( true );
+    if( !WiFi.config( INADDR_NONE, INADDR_NONE, INADDR_NONE )){
+      Serial.println( "STA Failed to unset configuration" );
     }
+    delay( 25 );
+    WiFi.begin( lightbarConfig.ssid, lightbarConfig.password );
+    Serial.println( "reconnecting" );
+  } else {
+    WiFi.reconnect();
+  }
 }
 
 void WiFiStationConnected( WiFiEvent_t event, WiFiEventInfo_t info ){
@@ -48,10 +48,10 @@ void WiFiStationConnected( WiFiEvent_t event, WiFiEventInfo_t info ){
 }
 
 void WiFiAPStaConnected( WiFiEvent_t event, WiFiEventInfo_t info ){
-    Serial.println( "Switching off station mode, AP only" );
-    WiFi.disconnect( true );
-    delay( 100 );
-    WiFi.mode( WIFI_MODE_AP );
+  Serial.println( "Switching off station mode, AP only" );
+  WiFi.disconnect( true );
+  delay( 100 );
+  WiFi.mode( WIFI_MODE_AP );
 }
 
 void initWiFi( void ){
@@ -81,26 +81,26 @@ void initWiFi( void ){
   } while( timeout && WiFi.status() != WL_CONNECTED );
   // not connected -> create hotspot
   if( WiFi.status() != WL_CONNECTED ) {
-      WiFi.disconnect( true );
+    WiFi.disconnect( true );
 
-      digitalWrite( lightbarConfig.gpioApMode, LOW );
+    digitalWrite( lightbarConfig.gpioApMode, LOW );
 
-      apName = String( "Lightbar " );
-      apName += WiFi.macAddress();
-      apName.replace( ":", "" );
+    apName = String( "Lightbar " );
+    apName += WiFi.macAddress();
+    apName.replace( ":", "" );
 
-      Serial.print( "\n\nCreating hotspot \"" );
-      Serial.print( apName.c_str() );
-      Serial.println( "\"" );
-      WiFi.mode( WIFI_MODE_APSTA );
-      delay( 25 );
-      WiFi.softAP( apName.c_str() );
-      WiFi.begin( lightbarConfig.ssid, lightbarConfig.password );
-      while( !WIFI_EVENT_AP_START ){ // wait until AP has started
-          delay( 100 );
-          Serial.print( "." );
-      }
-      WiFi.softAPConfig( softApIP, softApIP, IPAddress( 255, 255, 255, 0 ));
-      delay( 25 );
+    Serial.print( "\n\nCreating hotspot \"" );
+    Serial.print( apName.c_str() );
+    Serial.println( "\"" );
+    WiFi.mode( WIFI_MODE_APSTA );
+    delay( 25 );
+    WiFi.softAP( apName.c_str() );
+    WiFi.begin( lightbarConfig.ssid, lightbarConfig.password );
+    while( !WIFI_EVENT_AP_START ){ // wait until AP has started
+      delay( 100 );
+      Serial.print( "." );
     }
+    WiFi.softAPConfig( softApIP, softApIP, IPAddress( 255, 255, 255, 0 ));
+    delay( 25 );
+  }
 }
